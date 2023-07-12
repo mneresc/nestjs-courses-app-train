fd30f22619402fb059f860eda50927400bb28bb6
Neres <mneresc@gmail.com>
2023-07-12 07:45:23 -0300

    feat: add database suport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, TypeOrmModule.forRoot(
+   {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+   }
+  )],
   controllers: [AppController],
   providers: [],
 })
