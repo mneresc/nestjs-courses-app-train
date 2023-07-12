9e8c53ea1f3705e7c2f1da0a8a8eba955c6f6255
Neres <mneresc@gmail.com>
2023-07-12 08:45:06 -0300

    feat: config docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CoursesModule } from './courses/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 @Module({
   imports: [CoursesModule, TypeOrmModule.forRoot(
    {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true,
+    type: process.env.TYPEORM_CONNECTION as any,
+    host: process.env.HOST as any,
+    port: process.env.PORT as any,
+    username: process.env.USERNAME as any,
+    password: process.env.PASSWORD as any,
+    database: process.env.DATABASE as any,
+    autoLoadEntities: process.env.AUTOLOADENTITIES as any,
+    synchronize: process.env.SYNCHRONIZE as any,
    }
   )],
   controllers: [AppController],
